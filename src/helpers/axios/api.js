27e6a0caa762e4bf31622f2a177337fdd4546a53
D-Pushkarevskiy <descrew911@gmail.com>
2021-04-28 17:37:16 +0300

    init commit for gh-pages

--- src/helpers/axios/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-import config from '../config';
-
-export default axios.create({
-    baseURL: `https://api.unsplash.com/`,
-    headers: {
-        'Authorization': 'Client-ID ' + config.accessKey
-    }
-});
--- src/helpers/config.js
+++
@@ -1,5 +0,0 @@
-const config = {
-    accessKey: 'wwH-MTC0-LvCnLE7R23sO68Yaslks3lyRRI0Gp0iudA'
-}
-
-export default config;
